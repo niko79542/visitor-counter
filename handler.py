0380b2b1f152eea8ab1b52743b0522a6a1c2ed03
Nicholas Skoularikis <nick.skoularikis@gmail.com>
2022-05-26 21:12:37 -0400

    Switch to a views table with get and update endpoints.

--- handler.py
+++ handler.py
@@ -1,62 +1,25 @@
 import json
-import logging
 import boto3
 import datetime
 import dynamo
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 dynamodb = boto3.client('dynamodb')
-table_name = 'posts'
-
-
-def create(event, context):
-    logger.info(f'Incoming request is: {event}')
-
-    # Set the default error response
-    response = {
-        "statusCode": 500,
-        "body": "An error occured while creating post."
-    }
-
-    post_str = event['body']
-    post = json.loads(post_str)
-    current_timestamp = datetime.datetime.now().isoformat()
-    post['createdAt'] = current_timestamp
-
-    res = dynamodb.put_item(
-        TableName=table_name, Item=dynamo.to_item(post))
-
-    # If creation is successful
-    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
-        response = {
-            "statusCode": 201,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
-            },
-        }
-
-    return response
-
+table_name = 'views'
 
 def get(event, context):
-    logger.info(f'Incoming request is: {event}')
+    print(event)
     # Set the default error response
     response = {
         "statusCode": 500,
-        "body": "An error occured while getting post."
+        "body": "An error occured while getting view."
     }
 
-    post_id = event['pathParameters']['postId']
+    view_query = dynamodb.get_item(
+        TableName=table_name, Key={'id': {'N': "1"}})
 
-    post_query = dynamodb.get_item(
-        TableName=table_name, Key={'id': {'N': post_id}})
-
-    if 'Item' in post_query:
-        post = post_query['Item']
-        logger.info(f'Post is: {post}')
+    if 'Item' in view_query:
+        view = view_query['Item']
+        print(view)
         response = {
             "statusCode": 200,
             'headers': {
@@ -64,95 +27,43 @@ def get(event, context):
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS,GET'
             },
-            "body": json.dumps(dynamo.to_dict(post))
+            "body": json.dumps(dynamo.to_dict(view))
         }
-
-    return response
-
-
-def all(event, context):
-    logger.info(f'Incoming request is: {event}')
-
-    # Set the default error response
-    response = {
-        "statusCode": 500,
-        "body": "An error occured while getting all posts."
-    }
-
-    scan_result = dynamodb.scan(TableName=table_name)['Items']
-
-    posts = []
-
-    for item in scan_result:
-        posts.append(dynamo.to_dict(item))
-
-    response = {
-        "statusCode": 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,GET'
-        },
-        "body": json.dumps(posts)
-    }
 
     return response
 
 
 def update(event, context):
-    logger.info(f'Incoming request is: {event}')
-
-    post_id = event['pathParameters']['postId']
+    print(event)
 
     response = {
         "statusCode": 500,
-        "body": f"An error occured while updating post {post_id}"
+        "body": f"An error occured while updating view"
     }
-
-    post_str = event['body']
-
-    post = json.loads(post_str)
-
+    
     res = dynamodb.update_item(
         TableName=table_name,
         Key={
-            'id': {'N': post_id}
+            'id': {'N': "1"}
         },
-        UpdateExpression="set content=:c, author=:a, updatedAt=:u",
+        UpdateExpression="SET #views = if_not_exists(#views, :init) + :inc",
+        ExpressionAttributeNames={
+            "#views": "views"
+        },
         ExpressionAttributeValues={
-            ':c': dynamo.to_item(post['content']),
-            ':a': dynamo.to_item(post['author']),
-            ':u': dynamo.to_item(datetime.datetime.now().isoformat())
-        },
-        ReturnValues="UPDATED_NEW"
+            ":inc": {"N": '1'},
+            ":init": {'N': '0'}
+        }
     )
 
-    # If updation is successful for post
     if res['ResponseMetadata']['HTTPStatusCode'] == 200:
         response = {
-            "statusCode": 200,
+            "statusCode": 201,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,PUT'
+            },
         }
 
-    return response
-
-
-def delete(event, context):
-    logger.info(f'Incoming request is: {event}')
-
-    post_id = event['pathParameters']['postId']
-
-    # Set the default error response
-    response = {
-        "statusCode": 500,
-        "body": f"An error occured while deleting post {post_id}"
-    }
-
-    res = dynamodb.delete_item(TableName=table_name, Key={
-                               'id': {'N': post_id}})
-
-    # If deletion is successful for post
-    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
-        response = {
-            "statusCode": 204,
-        }
     return response
