ecac32352389f7f0f8df04d5f0a48f96a59a7297
Nicholas Skoularikis <nick.skoularikis@gmail.com>
2022-05-27 14:47:42 -0400

    Consistent quotation marks

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-sls deploy 
+### What is Lambda?  API Gateway? DynamoDB?
+
+
+
+
+[yaml](serverless.yml)
+```yaml
+service: views-api
+
+provider:
+  name: aws
+  runtime: python3.8
+  stage: dev
+  region: us-west-1
+  iamRoleStatements:
+    - Effect: Allow
+      Action:
+        - dynamodb:Query
+        - dynamodb:Scan
+        - dynamodb:GetItem
+        - dynamodb:PutItem
+        - dynamodb:UpdateItem
+        - dynamodb:DeleteItem
+      Resource: "arn:aws:dynamodb:us-west-1:797377682706:table/views"
+package:
+  exclude:
+    - node_modules/**
+    - venv/**
+```
+
+At the top of [serverless.yml](serverless.yml) you can name your api service, specify the language to use at runtime, envionment, and AWS region to use.  You can also designate an IAM role for actions the serverless functions will be able to perform.  Here we specify actions for our DynamoDB table, for our resource.  
+
+All of these options can be also setup in the AWS console under lambda/API Gateway, but it may be faster to use the CLI once you are more comfortable.  
+
+### Deploy to AWS 
+
+$ sls deploy 
 
--- handler.py
+++ handler.py
@@ -3,8 +3,8 @@ import boto3
 import datetime
 import dynamo
 
-dynamodb = boto3.client('dynamodb')
-table_name = 'views'
+dynamodb = boto3.client("dynamodb")
+table_name = "views"
 
 def get(event, context):
     print(event)
@@ -15,17 +15,17 @@ def get(event, context):
     }
 
     view_query = dynamodb.get_item(
-        TableName=table_name, Key={'id': {'N': "1"}})
+        TableName=table_name, Key={"id": {"N": "1"}})
 
-    if 'Item' in view_query:
-        view = view_query['Item']
+    if "Item" in view_query:
+        view = view_query["Item"]
         print(view)
         response = {
             "statusCode": 200,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,GET'
+            "headers": {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,GET"
             },
             "body": json.dumps(dynamo.to_dict(view))
         }
@@ -44,25 +44,25 @@ def update(event, context):
     res = dynamodb.update_item(
         TableName=table_name,
         Key={
-            'id': {'N': "1"}
+            "id": {"N": "1"}
         },
         UpdateExpression="SET #views = if_not_exists(#views, :init) + :inc",
         ExpressionAttributeNames={
             "#views": "views"
         },
         ExpressionAttributeValues={
-            ":inc": {"N": '1'},
-            ":init": {'N': '0'}
+            ":inc": {"N": "1"},
+            ":init": {"N": "0"}
         }
     )
 
-    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+    if res["ResponseMetadata"]["HTTPStatusCode"] == 200:
         response = {
             "statusCode": 201,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,PUT'
+            "headers": {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,PUT"
             },
         }
 
